fc20bed9f5b38316dcc290d433a415ee30f3f5c1
yorozuya-2003 <pagaria.2@iitj.ac.in>
2024-11-05 15:13:09 +0530

    reset route configurations

--- config/api.js
+++ config/api.js
@@ -3,6 +3,5 @@ module.exports = {
     defaultLimit: 25,
     maxLimit: 100,
     withCount: true,
-    prefix : '/v/blog-api/api'
   },
 };
--- config/server.js
+++ config/server.js
@@ -7,8 +7,4 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  url: '/v/blog-api',
-  admin: {
-    // url: '/v/blog-api/admin',
-  },
 });
